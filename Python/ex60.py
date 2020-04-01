a21a3bb2f25e23b6f25f207144e6a6eb612b4760
ThailerGraciano <58178803+ThailerGraciano@users.noreply.github.com>
2020-04-01 13:51:13 -0300

    Add files via upload

--- Python/ex60.py
+++ Python/ex60.py
@@ -1,5 +1,12 @@
-fatoral=int(input('Digite um número para descobrir seu fatorial: '))
-for cont in range(fatoral, 0, -1):
-    print(cont)
-    lista.append(cont)
-    print()
+from math import factorial
+print(100 * '¬')
+n = int(input('Digite um valor para descobrir seu fatorial: '))
+c = n
+f = factorial(n)
+print(100 * '¬')
+while c > 1:
+    print('{} x '.format(c), end='')
+    c -= 1
+if c == 1:
+    print('{} = {}'.format(c, f))
+print(100 * '¬')
